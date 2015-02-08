e364d4335c9279ad2846b1c97800fa98bbb97c6a
BlackAR <anthony.r.black13@gmail.com>
2015-02-08 14:35:32 -0700

    Board class

---
+++ src/Board.hx
@@ -0,0 +1,33 @@
+import flash.display.Sprite;
+import starling.display.Image;
+import Piece;
+
+class Board extends Sprite {
+	public var board:Array<Array<Piece>;
+
+	function new() {
+		super();
+		board = [[null, null, null, null], 
+				[null, null, null, null],
+				[null, null, null, null], 
+				[null, null, null, null]];
+		for (i in board){
+			for (j in board[i]){
+				board[i][j] = new Piece();
+			}
+		}
+
+	}
+
+	function display(){
+		var startX = 0;
+		var startY = 0;
+		for (i in board){
+			for(j in board[i]){
+				board[i][j].x = startX + 64*i;
+				board[i][j].y = startY + 65*j;
+				addChild(board[i][j]);
+			}
+		}
+	}
+}
