3bfefd748f0c6cc3e9ba1a694e9afba7d766e849
Erin Bailey <Erin@Erin-Baileys-MacBook-Pro.local>
2015-02-12 12:17:08 -0700

    Decreasing movesLeft

--- src/Scoreboard.hx
+++ src/Scoreboard.hx
@@ -14,17 +14,17 @@ class Scoreboard extends Sprite{
 	public function new(size:Int){
 		super();
 		score = 0;
-		movesLeft = Math.round((size*size)*.75);
+		movesLeft = Math.round((size*size)*.4);
 		scoreField = new TextField(100, 20, "Score: 0");
 		scoreField.vAlign = VAlign.TOP;
 		scoreField.hAlign = HAlign.LEFT;
 		movesField = new TextField(100, 20, "Moves Left: " + movesLeft);
 		movesField.vAlign = VAlign.TOP;
 		movesField.hAlign = HAlign.LEFT;
-		scoreField.x = 0;
-		scoreField.y = 0;
-		movesField.x = 0;
-		movesField.y = 20;
+		scoreField.x = 40;
+		scoreField.y = 40;
+		movesField.x = 40;
+		movesField.y = 60;
 		addChild(scoreField);
 		addChild(movesField);
 	}
@@ -36,7 +36,7 @@ class Scoreboard extends Sprite{
 
 	public function updateScore(size){
 		score += movesLeft * 10;
-		movesLeft = Math.round((size*size)*.75);
+		movesLeft = Math.round((size*size)*.4);
 		movesField.text = "Moves Left: " + movesLeft;
 		scoreField.text = "Score: " + score;
 	}
